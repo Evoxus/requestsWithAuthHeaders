8b0bbb8f8e48a1b0e7542cf87666a38b54a0c559
Michael Ploughman <michael@michaelhploughman.com>
2019-11-14 20:45:54 -0700

    Working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const URL = 'https://api.github.com/users/'; // /username/repos
 
 function getReposByUser(username) {
+  console.log(`${URL}${username}/repos`);
   fetch(`${URL}${username}/repos`)
     .then(res => res.json())
     .then(resJson => 
@@ -12,9 +13,17 @@ function getReposByUser(username) {
 
 function displayResults(res) {
   $('.results').html('');
-  let results = `<div>
-  </div>`
-  $('.results').append(results)
+
+  let results = [];
+  for (let i = 0; i < res.length; i++) {
+    results.push(`<div>
+    <h3>${res[i].name}</h3>
+    <a href="${res[i].html_url} target="_blank"">
+    <p>${res[i].description}</p>
+    </div>`)
+  };
+  console.log(results);
+  $('.results').append(...results);
 }
 
 function userInput() {
@@ -22,6 +31,11 @@ function userInput() {
     e.preventDefault();
     let input = $('#username').val()
     $('#username').val('');
+    getReposByUser(input);
+    console.log('button clicked');
   })
-  
 }
+
+$(function(){
+  userInput();
+})
